bb46280036a03a7e578e46cf4d757498a48b1291
DESKTOP-9K7BM14\PC <dangquy95php@gmail.com>
2023-08-19 11:28:49 +0700

    add text

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
   <!-- ======= Footer ======= -->
   <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright <strong><span>Cơ sở ma túy số 2</span></strong>. All Rights Reserved
+      &copy; Copyright <strong><span>Cơ sở cai nghiện ma túy số 2</span></strong>. All Rights Reserved
     </div>
     <div class="credits">
       <!-- All the links in the footer should remain intact. -->
