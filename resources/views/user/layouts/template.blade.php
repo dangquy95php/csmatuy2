15696af8a2c6ca56356c67e0ad5c58515c29335f
CS2-TNXP\dnquy <dangquy95php@gmail.com>
2024-12-25 15:39:47 +0700

    add

--- resources/views/user/layouts/template.blade.php
+++ resources/views/user/layouts/template.blade.php
@@ -176,7 +176,7 @@
         <!-- You can delete the links only if you purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-        Thiết kể bởi <a href="https://bootstrapmade.com/">Cơ sở cai nghiện ma túy số 2</a>
+        Thiết kể bởi <a href="">Cơ sở cai nghiện ma túy số 2</a>
         </div>
     </footer><!-- End Footer -->
 
