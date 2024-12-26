96c8637e5cf701b0453404c88d4b6124518b9a02
CS2-TNXP\dnquy <dangquy95php@gmail.com>
2024-12-26 07:44:00 +0700

    add

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -156,7 +156,37 @@
               </div>
             </div>
             <!-- End Customers Card -->
+            <div class="col-xxl-3 col-xl-12">
 
+              <div class="card info-card revenue-card">
+
+                <!-- <div class="filter">
+                  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                    <li class="dropdown-header text-start">
+                      <h6>Filter</h6>
+                    </li>
+
+                    <li><a class="dropdown-item" href="#">Today</a></li>
+                    <li><a class="dropdown-item" href="#">This Month</a></li>
+                    <li><a class="dropdown-item" href="#">This Year</a></li>
+                  </ul>
+                </div> -->
+
+                <div class="card-body">
+                  <h5 class="card-title">Tổng số phép chưa duyệt</h5>
+
+                  <div class="d-flex align-items-center">
+                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                      <a href=""><i class="ri-file-list-3-line"></i></a>
+                    </div>
+                    <div class="ps-3">
+                      <h6>5</h6>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              </div>
             <!-- Reports -->
             <!-- <div class="col-12">
               <div class="card">
